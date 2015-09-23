5f8848dea82ad272188f34f1d5b864401f682565
Hiswe <hiswehalya@gmail.com>
2015-09-23 21:14:12 +0200

    SVG – don't optimize

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 'use strict';
 
 var _                 = require('lodash');
+var path              = require('path');
 var gutil             = require('gulp-util');
 var GulpError         = gutil.PluginError;
 var through           = require('through2');
 var BPromise          = require('bluebird');
 
-var parseOptions      = require('./lib/parse-options.js');
+var defaults  				= require('./lib/default-config');
 var gatherDataFromSvg = require('./lib/raw-svg-data.js');
 var mergeDatas        = require('./lib/merge-datas.js');
 var renderTemplates   = require('./lib/render-templates.js');
 
 var PLUGIN_NAME       = 'gulp-svg-symbols';
+var BASE              = __dirname;
+var templates 				= {
+  'default-svg':  path.join(BASE, './templates/svg-symbols.svg'),
+  'default-css':  path.join(BASE, './templates/svg-symbols.css'),
+  'default-demo': path.join(BASE, './templates/svg-symbols-demo-page.html')
+};
 
 var plugin = function (opts) {
   var buffer  = [];
-  var options = parseOptions(opts);
+  // clone everything as we don't want to mutate anything
+  var options = _.defaults(_.cloneDeep(opts), _.cloneDeep(defaults));
+
+  // expand path to default templates
+  options.templates = options.templates.map(function(pathName) {
+  	if (pathName in templates) return templates[pathName];
+  	return pathname;
+  });
 
   return through.obj(function transform(file, encoding, cb) {
     if (file.isNull()) {
