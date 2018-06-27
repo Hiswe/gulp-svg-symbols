459f4f583605163577dbd864f4178d4ee4a42b48
Hiswe <hiswehalya@gmail.com>
2018-06-27 14:52:50 +0700

    TEMPLATES – vue – use the render function

--- templates/svg-symbols.vue
+++ templates/svg-symbols.vue
@@ -1,50 +1,59 @@
-<template>
-<div <%= attributesToString(svgAttrs) %>>
-  <%= icons.map(function(icon){
-    const symbolAttrs = attributesToString({
-      class: `svg-icon svg-icon--${icon.id}`,
-      viewBox: icon.svg.viewBox,
-      preserveAspectRatio: icon.svg.originalAttributes.preserveAspectRatio,
-    })
-    return `<svg${ symbolAttrs } :style="{'--svg-icon-scale': safeScale}" v-if="name === '${icon.id}'">${ icon.svg.content }</svg>`
-  }).join('\n') %>
-</div>
-</template>
-
 <style>
 :root {
   --svg-icon-scale: 1;
 }
 </style>
-<style scoped>
-<% _.forEach( icons, function( icon ){ %>.svg-icon--<%= icon.id %> {
-  width: calc(<%= icon.width %> * var(--svg-icon-scale));
-  height: calc(<%= icon.height %> * var(--svg-icon-scale));
-}<% if (icon.style) { %>
-<%= icon.style %><% } %>
-<% }); %>
-</style>
 
 <script>
+const SVG_LIB = {
+  <% _.forEach( icons, ( icon ) => { %>
+  "<%= icon.id %>": {
+    id: `<%= icon.id %>`,
+    style: {
+      width: `calc(<%= icon.width %> * var(--svg-icon-scale))`,
+      height: `calc(<%= icon.height %> * var(--svg-icon-scale))`,
+    },
+    content: `<%= icon.svg.content %>`,
+  },<% }); %>
+}
+
 export default {
   name: `svg-icon`,
   props: {
     name: {
-      default: ``,
-      type: String
-    },
-    scale: {
-      default: 1,
-      type: Number
+      type: String,
+      required: true,
     },
   },
   computed: {
     iconName() {
-      return <%= 'name' %> ? <%= '`svg-icon--${name}`' %> : '';
+      <%= 'return `svg-icon--${this.name}`' %>
     },
-    safeScale() {
-      return Number.isFinite( this.scale ) ? this.scale : 1;
+    style() {
+      if (!SVG_LIB[this.name]) return {}
+      return SVG_LIB[this.name].style
+    },
+    content() {
+      if (!SVG_LIB[this.name]) return ``
+      return SVG_LIB[this.name].content
+    },
+    icon() {
+      if (!SVG_LIB[this.name]) return {}
+      return SVG_LIB[this.name]
     }
-  }
+  },
+  render(createElement) {
+    if (!SVG_LIB[this.name]) return null
+    return createElement(
+      `svg`,
+      {
+        class: this.icon.id,
+        style: this.style,
+        domProps: {
+          innerHTML: this.content
+        },
+      }
+    )
+  },
 }
 </script>
